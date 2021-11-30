25f8e2771c462b3f51cb00531260b8c425a800a3
yukiyayoshida <92082599+yukiyayoshida@users.noreply.github.com>
2021-11-30 16:58:37 +0900

    triangle

---
+++ Triangle.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Triangle
+{
+    private float width;    //　横
+    private float height;   //　高さ
+
+    public Triangle(float w, float h)
+    {
+        this.width = w;
+        this.height = h;
+    }
+    public float GetSurface()
+    {
+        return (width * height / 2);
+    }
+}
